acaff1ff18755c0a94a968ee856a566141766b2f
lukaz1987 <90054564+lukaz1987@users.noreply.github.com>
2022-02-16 22:51:44 +0100

    Add files via upload

---
+++ domoticz_stan_czujnikow_email_prod.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib.request
+import smtplib
+import time
+
+adres = "http://ip.ip.ip.ip:port/json.htm?type=devices&rid="
+czujniki = {"Kuchnia (góra)": 9, 
+            "Strych": 10, 
+            "Spiżarka (dół)": 11,
+            "Kuchnia (dół)": 12,
+            "Temp+wilg_Kuchnia (dół)": 13,
+            "Temp+wilg_Na_Polu": 14,
+            "Boiler CS": 20,
+            "Piec": 21,
+            "Pokój I (dół)": 22,
+            "Na polu": 23,
+            "Halogen_Wifi": 35,
+            "MQ9_Wifi": 36,
+            "Multi_sensors_Wifi": 37,
+            "Pokój II (dół)": 42,
+            "Spiżarka (góra)": 43,
+            "Pokój IV (góra)": 44,
+            "Pokój III (góra)": 45,
+            "Na polu (W)": 46,
+            "Boiler CO": 47,
+            "DS18b20_Wifi": 50,
+            "Old_Home": 73
+}
+
+
+gmail_user = '@gmail.com'
+gmail_password = 'haslo'
+
+sent_from = gmail_user
+to = ['@gmail.com']
+
+x = 0
+while True:
+    for key in czujniki:
+        idx = str(czujniki[key])
+        url = adres + str(czujniki[key])
+        json_text = urllib.request.urlopen(url).read().decode()
+        data = json.loads(json_text)
+        data2 = data['result']
+
+
+        for data3 in data2:
+            print(data3['LastUpdate'])
+
+
+        czas_serwera = data['ServerTime']
+        czas_aktualizacji = data3['LastUpdate']
+
+
+        if czas_serwera[14:16] == czas_aktualizacji[14:16]:
+            print('Czas jest ten sam')
+        else:
+            print('Czas jest inny')
+
+
+        minuty_serwera = int(czas_serwera[14:16])
+        minuty_aktualizacji = int(czas_aktualizacji[14:16])
+        roznica_minut = minuty_serwera - minuty_aktualizacji
+
+
+        tresc_subject = key + ": "
+        subject = tresc_subject
+        tresc_body = "Czas serwera: " + str(czas_serwera) + " a czas aktualizacji czujnika: " + str(czas_aktualizacji)
+        body = tresc_body
+        email_text = "From: %s\nTo: %s\nSubject: %s\n\n%s" % (sent_from, to, subject, body)
+
+        if roznica_minut >= 6 or roznica_minut <= -1:
+            print('Różnica minut jest więkasza o', roznica_minut,  'minut/e!!!')
+            try:
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                server.login(gmail_user, gmail_password)
+                server.sendmail(sent_from, to, email_text.encode("utf8"))
+                server.close()
+
+                print('Email sent!')
+            except:
+                print('Something went wrong...')
+        else:
+            print('Różnica minut jest większa o', roznica_minut,'minut/e ale nie wieksza od 6!!!')
+
+        x = x + 1
+        print("petla", x)
+        print(email_text)
+        time.sleep(5)
